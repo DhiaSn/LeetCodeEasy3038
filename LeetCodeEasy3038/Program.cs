3da592e0c402284a9e2a1b48d9a251404c208085
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-07-30 14:58:02 +0100

    first init solution

--- LeetCodeEasy3038/Program.cs
+++ LeetCodeEasy3038/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int result1 = Solution.MaxOperations([3, 2, 1, 4, 5]); 
+            if (result1 != 2)
+                Console.WriteLine($"result1 is: {result1} instead of 2");
+
+            int result2 = Solution.MaxOperations([3, 2, 6, 1, 4]);
+            if (result2 != 1)
+                Console.WriteLine($"result2 is: {result2} instead of 1");
+
+            int result3 = Solution.MaxOperations([4, 3, 3, 4, 10]);
+            if (result3 != 2)
+                Console.WriteLine($"result3 is: {result3} instead of 2");
+
+            Console.WriteLine($"it works perfectly!");
         }
     }
 }
---
+++ LeetCodeEasy3038/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeEasy3038
+{
+    public static class Solution
+    {
+        public static int MaxOperations(int[] nums)
+        {
+            if(nums.Length < 2) return 0; 
+            if(nums.Length == 2) return 1;
+
+            int lastScore = nums[0] + nums[1];
+            int tempScore = nums[0] + nums[1];
+            int result = 0;
+            int[] tempList = nums;
+            while(tempScore == lastScore)
+            {
+                result++;
+                int[] newList = new int[tempList.Length - 2];
+                if (newList.Length >= 2)
+                {
+                    int j = 0;
+                    for (int i = 2; i < tempList.Length; i++)
+                    {
+                        newList[j] = tempList[i];
+                        j++;
+                    }
+                    tempScore = newList[0] + newList[1];
+                    tempList = newList;
+                }
+                else
+                    break; 
+            }
+
+            return result; 
+        }
+    }
+}
