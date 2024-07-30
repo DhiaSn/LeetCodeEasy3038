fbb2ba2170e9051a39b98bc7319f8322f570bc27
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-07-30 19:56:05 +0100

    improving code

--- LeetCodeEasy3038/Program.cs
+++ LeetCodeEasy3038/Program.cs
@@ -6,16 +6,23 @@
         {
             int result1 = Solution.MaxOperations([3, 2, 1, 4, 5]); 
             if (result1 != 2)
+            {
                 Console.WriteLine($"result1 is: {result1} instead of 2");
-
+                return; 
+            }
             int result2 = Solution.MaxOperations([3, 2, 6, 1, 4]);
             if (result2 != 1)
+            {
                 Console.WriteLine($"result2 is: {result2} instead of 1");
+                return;
+            }
 
             int result3 = Solution.MaxOperations([4, 3, 3, 4, 10]);
             if (result3 != 2)
+            {
                 Console.WriteLine($"result3 is: {result3} instead of 2");
-
+                return;
+            }
             Console.WriteLine($"it works perfectly!");
         }
     }
